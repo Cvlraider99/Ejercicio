ec03160a6be2e3d4e3e635c1f2869620b183c090
Cvlraider99 <al.galrey@gmail.com>
2022-01-26 21:16:45 -0600

    Ya se quita de la lista original los eliminados

--- lib/listaFunciones/sacarLista.dart
+++ lib/listaFunciones/sacarLista.dart
@@ -1,6 +1,7 @@
 //En este archivo se van a sacar los datos de la API
 import 'dart:convert';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ejercicio/listaFunciones/imprimirLista.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
@@ -10,11 +11,31 @@ import 'package:flutter/material.dart';
 class SacarLista extends StatelessWidget {
   //Esta funcion me conectara a la Api y sacara toda la informacion
   Future <List> infoApi() async{
-    var listaDetalles = [];
+    var listaDetalles = []; //Lista Final
+    var listaEliminados = [];//Lista de eliminados
+
     final respuestaApi = await http.get (Uri.parse("https://pokeapi.co/api/v2/pokemon/"));
     var infoApi = json.decode(respuestaApi.body);
     for (int i = 0; i<infoApi["results"].length; i++){
       listaDetalles.add(infoApi["results"][i]);
+    }
+
+    //Ahora se compara la lista original con la lista de Eliminados y elimina las coincidencias
+    await FirebaseFirestore.instance.collection("eliminados").get().then((QuerySnapshot doc) => {
+      doc.docs.forEach((doc) {
+        listaEliminados.add(doc["Name"]);
+      })
+    });
+
+    for (int i=0; i<listaEliminados.length; i++)
+    {
+      for (int a=0; a<listaDetalles.length; a++)
+      {
+        if (listaEliminados[i] == listaDetalles[a]["name"])
+        {
+          listaDetalles.removeAt(a);
+        }
+      }
     }
     return listaDetalles;
   }
